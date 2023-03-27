4973fb535f5f4a614fa0346ab1c20549b0657254
Piotr Adamowicz <pad@novicell.co.uk>
2023-03-27 12:28:08 +0200

    chore: added grid layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+
 import TodoList from '@/components/TodoList/TodoList';
 import { Container } from '@mui/material';
 import Counter from '@/components/Counter/Counter';
 import AddTodo from '@/components/todos/AddTodo';
+import Grid from '@mui/material/Grid';
 
 const Home = () => {
   return (
     <Container sx={{ py: 2, position: 'relative' }}>
-      <AddTodo />
-      <TodoList />
-      <Counter />
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <AddTodo />
+        </Grid>
+        <Grid item xs={12}>
+          <TodoList />
+        </Grid>
+        <Counter />
+      </Grid>
     </Container>
   );
 };
