d2f0528b04850d5c6bad8d09f3ed793d827862d1
Piotr Adamowicz <pad@novicell.co.uk>
2023-03-27 12:32:57 +0200

    feature: added validation for empty string

--- src/components/todos/AddTodo.tsx
+++ src/components/todos/AddTodo.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
+
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Validate, ValidationGroup } from 'mui-validate';
 
 const AddTodo = () => {
-  const valid = false;
-
   return (
     <Box
       component="form"
       sx={{
         '& .MuiTextField-root': { m: 2, ml: 0, width: '100%' },
       }}
-      noValidate
       autoComplete="off"
     >
-      <TextField
-        error={valid}
-        id="outlined-multiline-flexible"
-        label="Add task"
-        defaultValue=""
-        helperText={`${valid ? 'Incorrect entry.' : ''}`}
-        fullWidth
-      />
+      <ValidationGroup>
+        <Validate name="addTask" required={[true, 'Task must not be empty']}>
+          <TextField fullWidth />
+        </Validate>
+      </ValidationGroup>
     </Box>
   );
 };
