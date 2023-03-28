3eeb8101f11cc991ec3e0973c7a2b6b2ee9be1fa
Piotr Adamowicz <pad@novicell.co.uk>
2023-03-28 15:26:15 +0200

    bugfix: update reducer data

--- src/slices/todosSlices.ts
+++ src/slices/todosSlices.ts
@@ -40,7 +40,7 @@ const todosSlice = createSlice({
       const isEqual = (el: Todo): boolean => el.id === action.payload.id;
       const id = state.todos.findIndex(isEqual);
       console.log(id);
-      state.todos[id].text = state.todos[id].text + action.payload.value;
+      state.todos[id].text = action.payload.value;
     },
     toggleStateById: (state, action: PayloadAction<Todo>) => {
       const { id } = action.payload;
