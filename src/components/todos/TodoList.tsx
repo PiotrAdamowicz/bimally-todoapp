774c47793bf1494a1094e8e40f84aeea7b15729f
Piotr Adamowicz <pad@novicell.co.uk>
2023-03-29 10:39:38 +0200

    styling: small RWD adjustments

--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -25,15 +25,18 @@ import SaveAltIcon from '@mui/icons-material/SaveAlt';
 //Utilities
 import { lightBlue } from '@mui/material/colors';
 import { Todo } from '@/types/todos';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const TodoList = () => {
   const list = useSelector(selectTodos);
   const dispatch = useDispatch();
 
+  const matches = useMediaQuery('(max-width:600px)');
+
   return (
     <Paper elevation={12}>
       <Box sx={{ width: '100%' }}>
-        <List sx={{ p: 0, width: '100%', bgcolor: lightBlue[900] }} dense>
+        <List sx={{ p: 0, width: '100%', bgcolor: lightBlue[900] }} dense={matches ? true : false}>
           {list.todos.map((todo: Todo, index: number) => (
             <ListItem
               key={todo.id}
@@ -52,7 +55,7 @@ const TodoList = () => {
               <ListItemButton role={undefined}>
                 <ListItemIcon sx={{ minWidth: '30px' }}>
                   <Checkbox
-                    sx={{ m: 0, p: 0 }}
+                    sx={matches ? { m: 0 } : { m: 3, p: 0 }}
                     edge="start"
                     size="small"
                     checked={todo.isDone}
@@ -64,7 +67,7 @@ const TodoList = () => {
 
                 {todo.editActive ? (
                   <Box sx={{ width: '70%' }}>
-                    <Input edit variant="edit" id={todo.id} label={<SaveAltIcon />} text={todo.text} />
+                    <Input variant="edit" id={todo.id} label={<SaveAltIcon />} text={todo.text} />
                   </Box>
                 ) : (
                   <Box>
